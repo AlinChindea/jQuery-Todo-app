1b81cd3e6226253a0956c5aef35c88b042429d03
Alin Chindea <alin.chindea@gmail.com>
2018-04-17 13:58:46 +0100

    cross out completed task on click and re-activate it if not completed by clicking again

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 $(() => {
-  console.log('jQuery loaded');
+  //check off specific todos on click
+  const $lis = $('li');
+
+  $lis.on('click', function(){
+    $(this).toggleClass('completed');
+  });
 
 
 });
