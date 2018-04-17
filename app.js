89a765c17110d8fedd8dc941d7b83e3fe0cf0295
Alin Chindea <alin.chindea@gmail.com>
2018-04-17 14:33:52 +0100

    added logic to grab user's input - new todo - and add and/or remove it from the list

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 $(() => {
   //check off specific todos on click
-  const $lis = $('li');
-  const $span = $('span');
+  const $ul= $('ul');
+  const $userInput = $('input[type="text"]');
 
-  $lis.on('click', function(){
+  $ul.on('click', 'li', function(){
     $(this).toggleClass('completed');
   });
 
-  //click on X to delete Todo
-  $span.on('click', function(e){
+  //click to delete Todo
+  $ul.on('click', 'span', function(e){
     $(this).parent().fadeOut(500, function(){
       $(this).remove(); //this refers here to the li
     });
     e.stopPropagation();
   });
+
+  $userInput.on('keypress', function(e){
+    if(e.which === 13){
+      //grabbing new todo text from user input
+      const $newTodo = $(this).val();
+      $(this).val('');
+      //create a new li and add add to ul
+      $ul.append(`<li><span>X</span> ${$newTodo}</li>`);
+    }
+
+  });
+
+
 
 
 });
