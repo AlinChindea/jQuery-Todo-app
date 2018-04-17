f1b591c04099715705ed4e1839b6be03231ff2de
Alin Chindea <alin.chindea@gmail.com>
2018-04-17 14:15:13 +0100

    added logic to remove a todo when user clicks on X

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 $(() => {
   //check off specific todos on click
   const $lis = $('li');
+  const $span = $('span');
 
   $lis.on('click', function(){
     $(this).toggleClass('completed');
+  });
+
+  //click on X to delete Todo
+  $span.on('click', function(e){
+    $(this).parent().fadeOut(500, function(){
+      $(this).remove(); //this refers here to the li
+    });
+    e.stopPropagation();
   });
 
 
